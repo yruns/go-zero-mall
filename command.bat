ffa4a53798337b45eab025b6ef1917bbe14b25e7
yruns <yruns.sh@qq.com>
2023-06-09 04:54:50 +0800

    add service

--- command.bat
+++ command.bat
@@ -3,3 +3,19 @@
 @REM goctl rpc protoc user.proto --go_out=./types --go-grpc_out=./types --zrpc_out=.
 
 go run product.go -f etc/product.yaml
+
+@REM 启动product rpc
+go run product/rpc/product.go -f product/rpc/etc/product.yaml
+@REM 启动product api
+go run product/api/product.go -f product/api/etc/product.yaml
+
+@REM 启动user rpc
+go run user/rpc/user.go -f user/rpc/etc/user.yaml
+@REM 启动user api
+go run user/api/user.go -f user/api/etc/user.yaml
+
+@REM 启动order rpc
+go run order/rpc/order.go -f order/rpc/etc/order.yaml
+@REM 启动order api
+go run order/api/order.go -f order/api/etc/order.yaml
+
